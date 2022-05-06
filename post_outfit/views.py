ef8e3939884a975a3e71c0ecca45bb6811953079
raylicola <rope.champ.109@gmail.com>
2022-05-06 10:37:32 +0900

    ログイン機能の実装

--- post_outfit/views.py
+++ post_outfit/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    params = {
-        'title': 'Hello'
-    }
-    return render(request, 'post_outfit/index.html', params)
+
+    return render(request, 'post_outfit/index.html')
+
+def register(request):
+    return render(request, 'post_outfit/register.html')
